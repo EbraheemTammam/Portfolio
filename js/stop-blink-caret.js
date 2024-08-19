92b86cb8e852aa8fa6ce38ecc7839535e2aea9fa
Ebraheem <ebraheem20012020@gmail.com>
2024-08-19 17:54:17 +0300

    my-name animatiion

---
+++ js/stop-blink-caret.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const typewriter = document.querySelector('.my-name');
+
+    setTimeout(() => {
+      typewriter.classList.add('typing-done');
+    }, 3500);
+  };
